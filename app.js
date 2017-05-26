4fd41812d9d8a38fb99599754a7a5390bc59cc72
eli goldweber <eligoldweber@gmail.com>
2017-05-26 17:24:26 -0400

    test

--- app.js
+++ app.js
@@ -24,27 +24,3 @@ serv.listen(process.env.PORT || 2000);
 
 console.log("hello world");
 
-const TelegramBaseController = Telegram.TelegramBaseController
-const TextCommand = Telegram.TextCommand
-const tg = new Telegram.Telegram('331685887:AAGbp2P-Fm0FKnrBOMOiC5pC4bGdffG5Nxg')
-
-class PingController extends TelegramBaseController {
-    /**
-     * @param {Scope} $
-     */
-    pingHandler($) {
-        $.sendMessage('pong')
-    }
-
-    get routes() {
-        return {
-            'pingCommand': 'pingHandler'
-        }
-    }
-}
-
-tg.router
-    .when(
-        new TextCommand('ping', 'pingCommand'),
-        new PingController()
-    )
