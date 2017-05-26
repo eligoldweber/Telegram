d587c4b54d4572336e128c9c8fb639653418886c
eli goldweber <eligoldweber@gmail.com>
2017-05-26 16:33:50 -0400

    test

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var serv = require('http').Server(app);
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/client/index.html');
 });
+
+app.get('/test',function(req,res){
+	res.sendFile(__dirname + '/client/test.html');
+});
 app.use('/client',express.static(__dirname + '/client'));
 serv.listen(process.env.PORT || 2000);
 
