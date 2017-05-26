4f5c0d8d63070294a61a21f799870184d79ba483
eli goldweber <eligoldweber@gmail.com>
2017-05-26 17:22:39 -0400

    test

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var express = require('express');
 var telegram = require('telegram-node-bot');
 var app = express();
@@ -21,3 +23,28 @@ serv.listen(process.env.PORT || 2000);
 
 
 console.log("hello world");
+
+const TelegramBaseController = Telegram.TelegramBaseController
+const TextCommand = Telegram.TextCommand
+const tg = new Telegram.Telegram('331685887:AAGbp2P-Fm0FKnrBOMOiC5pC4bGdffG5Nxg')
+
+class PingController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    pingHandler($) {
+        $.sendMessage('pong')
+    }
+
+    get routes() {
+        return {
+            'pingCommand': 'pingHandler'
+        }
+    }
+}
+
+tg.router
+    .when(
+        new TextCommand('ping', 'pingCommand'),
+        new PingController()
+    )
