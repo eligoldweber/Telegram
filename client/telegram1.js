239eec0ccb47c9ae74f1c9881d9cce509f785b4c
eli goldweber <eligoldweber@gmail.com>
2017-05-27 12:31:22 -0400

    test

--- client/telegram1.js
+++ client/telegram1.js
@@ -1,5 +1,11 @@
 
 
+const Nexmo = require('nexmo');
+const nexmo = new Nexmo({
+  apiKey: '0e2b809d',
+  apiSecret: 'f12d2fc77a8b42cd'
+});
+
 var TelegramBot = require('node-telegram-bot-api');
 
 var token = '331685887:AAGbp2P-Fm0FKnrBOMOiC5pC4bGdffG5Nxg';
@@ -13,22 +19,28 @@ bot.on('message', function(msg){
 	
 	var echo = msg.text;
 	
-	bot.sendMessage(id, echo);
-	const Nexmo = require('nexmo');
-	const nexmo = new Nexmo({
-	  apiKey: '0e2b809d',
-	  apiSecret: 'f12d2fc77a8b42cd'
-	});
-	nexmo.message.sendSms(
-	  '12034568758', '15136935944', 'yo',
-	    (err, responseData) => {
-	      if (err) {
-	        console.log(err);
-			//console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-	      } else {
-	        console.dir(responseData);
-	      }
-	    }
-	 );
-
+	switch(echo){
+		case '/test':
+			bot.sendMessage(id, echo);
+			break;
+		case '/text_msg':
+			nexmo.message.sendSms(
+			  '12034568758', '15136935944', 'yo \n',
+			    (err, responseData) => {
+			      if (err) {
+			        console.log(err);
+					//console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			      } else {
+			        console.dir(responseData);
+			      }
+			    }
+			 );
+			 break:
+		 default:
+			bot.sendMessage(id, "you smell");
+	}
+	
+	
+	
+	
 });
