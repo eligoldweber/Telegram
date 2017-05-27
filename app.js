5f1f344b7f36c88ca8ecd8693dc08e12b5e9d6b0
eli goldweber <eligoldweber@gmail.com>
2017-05-27 12:04:37 -0400

    test

--- app.js
+++ app.js
@@ -22,18 +22,5 @@ serv.listen(process.env.PORT || 2000);
 
 console.log("hello world");
 
-// var TelegramBot = require('node-telegram-bot-api');
-//
-// var token = '331685887:AAGbp2P-Fm0FKnrBOMOiC5pC4bGdffG5Nxg';
-//
-// var opt = {polling: true};
-// var bot = new TelegramBot(token, opt);
-//
-// bot.on('message', function(msg){
-// 	console.log(msg);
-// 	var id = msg.chat.id;
-//
-// 	var echo = msg.text;
-//
-// 	bot.sendMessage(id, echo);
-// });
+
+  
--- client/telegram1.js
+++ client/telegram1.js
@@ -1,3 +1,5 @@
+
+
 var TelegramBot = require('node-telegram-bot-api');
 
 var token = '331685887:AAGbp2P-Fm0FKnrBOMOiC5pC4bGdffG5Nxg';
@@ -12,4 +14,21 @@ bot.on('message', function(msg){
 	var echo = msg.text;
 	
 	bot.sendMessage(id, echo);
+	const Nexmo = require('nexmo');
+	const nexmo = new Nexmo({
+	  apiKey: '0e2b809d',
+	  apiSecret: 'f12d2fc77a8b42cd'
+	});
+	nexmo.message.sendSms(
+	  '12034568758', '15136935944', 'yo',
+	    (err, responseData) => {
+	      if (err) {
+	        console.log(err);
+			//console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	      } else {
+	        console.dir(responseData);
+	      }
+	    }
+	 );
+
 });
